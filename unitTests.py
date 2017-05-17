7c4d1495a4b64237b21e3ea1cc416d679c90a4f8
Felix Jancso-Szabo <felix.ajs@gmail.com>
2017-05-17 19:34:08 -0400

    Changed the function names to be more descriptive

--- unitTests.py
+++ unitTests.py
@@ -25,24 +25,24 @@ class TestBracketNestMethods(unittest.TestCase):
         self.assertFalse(checkBracketParity("stringpart1(stringpart2&stringpart3"))
 
 class TestBoolAlgParser(unittest.TestCase):
-    def test_1(self):
+    def test_expression(self):
         testing = boolAlg("(A+D)(BD)'+(AC+D')'")
         self.assertEquals("(A or D) and not (B and D) or not (A and C or not D)", testing.exp)
         # "(A+D)(BD)'+(AC+D')'"
 
-    def test_2(self):
+    def test_negated_closing_brackets(self):
         testing = boolAlg("A+(B(C+(D(E+(F)')')')')'")
         self.assertEquals("A or not (B and not (C or not (D and not (E or not (F)))))", testing.exp)
 
-    def test_3(self):
+    def test_closing_brackets(self):
         testing = boolAlg("A+(B(C+(D(E+(F)))))")
         self.assertEquals("A or (B and (C or (D and (E or (F)))))", testing.exp)
 
-    def test_4(self):
+    def test_opening_brackets(self):
         testing = boolAlg("(((((A)+B)C)+D)E)+FG")
         self.assertEquals("(((((A) or B) and C) or D) and E) or F and G", testing.exp)
 
-    def test_5(self):
+    def test_negated_opening_brackets(self):
         testing = boolAlg("(((((A)'+B)'C)'+D)'E)'+F'G'")
         self.assertEquals("not ( not ( not ( not ( not (A) or B) and C) or D) and E) or not F and not G", testing.exp)
 
